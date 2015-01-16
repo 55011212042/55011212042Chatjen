9dbf51b7e1d62a703d2d931dfb5d049023842cf8
55011212042 <istudents@iClassrooms-iMac-36.local>
2015-01-16 15:57:56 +0700

    test
    test

--- JirapanProject2.playground/section-1.swift
+++ JirapanProject2.playground/section-1.swift
@@ -2,4 +2,16 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var shoppingListExplicit = [String]()
+shoppingListExplicit = ["Eggs", "Milk"]
+// var shoppingListExplicit: [String] = ["Eggs", "Milk"]
+
+var shoppingList = ["Eggs", "Milk"]
+println("The shopping list contains \(shoppingList.count)")
+shoppingList.append("Flour")
+shoppingList += "Baking Powder"
+shoppingList += ["Chocolate Spread", "Cheese", "Butter"]
+var firsItem = shoppingList[0]
+shoppingList[0] = "Six eggs"
+//shoppingList[0...3] = ["Bananas", "Apples"]
+shoppingList
