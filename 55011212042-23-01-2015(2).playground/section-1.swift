3bb8dea6dd8216b8b1e90bb68072422544a1d55e
55011212042 <istudents@iClassrooms-iMac-36.local>
2015-01-23 14:00:09 +0700

    week 2
    week 2

---
+++ 55011212042-23-01-2015(2).playground/section-1.swift
@@ -0,0 +1,32 @@
+class TipCalculator {
+    //2
+    let total: Double
+    let taxPct: Double
+    let subtotal: Double
+    //3
+    init(total: Double, taxPct: Double) {
+        self.total = total
+        self.taxPct = taxPct
+        subtotal = total / (taxPct + 1)
+    }
+    //4
+    func calcTipWithTipPct(tipPct: Double) -> Double {
+        return subtotal * tipPct
+    }
+    //1
+    func returnPossibleTips() -> [Int: Double] {
+        let possibleTipsInferred = [0.15, 0.18, 0.20]
+        let possibleTipsExplicit: [Double] = [0.15, 0.18, 0.20]
+        //2
+        var retval = Dictionary<Int, Double>()
+        for possiblTip in possibleTipsInferred {
+            let intPct = Int(possiblTip*100)
+            //3
+            retval [intPct] = calcTipWithTipPct(possiblTip)
+        }
+        return retval
+    }
+}
+//6
+let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
+tipCalc.returnPossibleTips()
