5b1c15f9f748da16c688318e9b38630dccab02ab
55011212042 <istudents@iClassrooms-iMac-36.local>
2015-01-23 15:30:06 +0700

    week2
    week2

--- 55011212042-23-01-2015(3).playground/section-1.swift
+++ 55011212042-23-01-2015(3).playground/section-1.swift
@@ -2,16 +2,16 @@ class Zoo {
     let animals:[String] = ["Dog","Cat","Fish"]
     let feed:[String] = ["Pedigree","Whiskas","Sakura"]
     
-    let animal:String
+    let animal2:String
     
     init(animal: String) {
-        self.animal=animal
+        self.animal2=animal
     }
     
     func processing() -> String {
         var ans:String = ""
         for(var i=0 ; i<animals.count ; i++){
-            if(animals[i]==animal){
+            if(animals[i]==animal2){
                 ans=feed[i]
             }
         }
