905d73280d831b7c1050308640b1e5764d66a93f
55011212042 <istudents@iClassrooms-iMac-36.local>
2015-01-30 16:17:20 +0700

    week3
    week3

--- 55011212042-30-01-2015(3).playground/section-1.swift
+++ 55011212042-30-01-2015(3).playground/section-1.swift
@@ -38,6 +38,46 @@ class Calculator {
             println("Invalid parameter (\(num1)),(\(num2)) number must be >= 0 and < 100.")
         }
     }
+    func calculate2(num1Input:Double, num2Input:Double, op:String){
+        if(num1Input>(-100) && num2Input<100 && num1Input<100 && num2Input>(-100)){
+            var ans:Double
+            if(op == "+"){
+                ans = num1Input + num2Input
+                println("\(num1Input) + \(num2Input) = \(ans)")
+            }
+            else if(op == "-"){
+                ans = num1Input - num2Input
+                println("\(num1Input) - \(num2Input) = \(ans)")
+            }
+            else if(op == "*"){
+                ans = num1Input * num2Input
+                println("\(num1Input) * \(num2Input) = \(ans)")
+            }
+            else if(op == "/"){
+                ans = num1Input / num2Input
+                println("\(num1Input) / \(num2Input) = \(ans)")
+            }
+            else if(op == "%"){
+                ans = num1Input % num2Input
+                println("\(num1Input) % \(num2Input) = \(ans)")
+            }
+            else{
+                println("Operator is not correct!")
+            }
+        }
+        else{
+            println("Invalid parameter (\(num1)),(\(num2)) number must be >= 0 and < 100.")
+        }
+    }
 }
 let cal=Calculator(num1Input:4,num2Input:10)
 cal.calculate("/")
+cal.calculate("*")
+cal.calculate("%")
+cal.calculate("-")
+cal.calculate("+")
+cal.calculate2(50, num2Input: 75, op: "/")
+cal.calculate2(60, num2Input: 50, op: "+")
+cal.calculate2(30, num2Input: 50, op: "-")
+cal.calculate2(40, num2Input: 50, op: "*")
+cal.calculate2(90, num2Input: 50, op: "%")
