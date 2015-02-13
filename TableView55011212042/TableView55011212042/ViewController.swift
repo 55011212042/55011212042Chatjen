3aacbf929917281f5d353a8f9c8cc2f9e974ea57
55011212042 <istudents@iClassrooms-iMac-36.local>
2015-02-13 15:24:36 +0700

    week5
    week5

---
+++ TableView55011212042/TableView55011212042/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  TableView55011212042
+//
+//  Created by iStudents on 2/13/15.
+//  Copyright (c) 2015 iStudents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate {
+    
+    let cellIdentifier="cellIdentifier"
+    var tableData=["bell46","camera90","email109","calendar166"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var tableView: UITableView!
+    
+// Add
+    func numberOfSelectionInTableView(tableView: UITableView!) -> Int {
+        return 1
+    }
+    func tableView(tableView: UITableView!, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+        var cell=tableView!.dequeueReusableCellWithIdentifier(self.cellIdentifier) as UITableViewCell
+        cell.textLabel!.text=self.tableData[indexPath.row]
+        var imageName = UIImage(named: tableData[indexPath.row])
+        cell.imageView!.image = imageName
+        return cell
+    }
+    func tableView(tableView: UITableView!, didSelectRowAtIndexPath indexPath: NSIndexPath!) {
+        let alert=UIAlertController(title: "Item selected", message: "You selected item \(tableData[indexPath.row])", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: UIAlertActionStyle.Default,
+                                      handler: {
+                                            (alert: UIAlertAction!) in println("An alert of type \(alert.style.hashValue) was tapped!")
+                                      }))
+
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+}
+
